dda4c6e4f200af7c45fbb04f4c76c4bcb81ba4a6
Daniel Saper <daniel.saper@outlook.com>
2018-09-23 18:29:43 -0400

    Sass and more components

--- src/game_client/components/ResourceList.tsx
+++ src/game_client/components/ResourceList.tsx
@@ -1,21 +1,17 @@
 import * as React from "react";
 import { Resources } from "../../common/GameStateModels";
+import { ResourceLine } from "./ResourceLine";
 
 export interface IResouceListModel {
   resources: Resources;
 }
 
-export class ResourceList extends React.Component<IResouceListModel, {}> {
+export class ResourceList extends React.PureComponent<IResouceListModel, {}> {
   public render() {
     return (
       <ul className="resource_lines">
         {Array.from(this.props.resources.entries()).map(([resourceType, resource]) => {
-          return (
-            <li className="resource_line" key={resourceType}>
-              <div className="resource_line_entry_name">{resourceType}:</div>
-              <div className="resource_line_count">{resource.toString()}</div>
-            </li>
-          );
+          return <ResourceLine name={resourceType} value={resource} />;
         })}
       </ul>
     );

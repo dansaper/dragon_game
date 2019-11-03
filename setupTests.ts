ee45a199a6c32b12cea787860569b207f4ab5526
Daniel Saper <daniel.saper@outlook.com>
2019-11-03 00:22:08 -0400

    Updating dependencies

--- setupTests.ts
+++ setupTests.ts
@@ -1,7 +1,5 @@
 // add some helpful assertions
 import "@testing-library/jest-dom/extend-expect";
-// this is basically: afterEach(cleanup)
-import "@testing-library/react/cleanup-after-each";
 
 // Mock out requestAnimationFrame in the easiest way we can
 beforeEach(() => {
